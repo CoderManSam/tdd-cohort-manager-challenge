5f4a210b4a78037e7f4d66163825e9a9cea14b32
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-06-13 17:58:38 +0100

    adding completed standard criteria files

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -33,6 +33,14 @@ describe("Cohort", () => {
         expect(result).toEqual(expected);
     });
 
+    it("When searching for a cohort by cohort name, return an error message if the cohort cannot be found", () => {
+        cohortManager.addCohort(cohort1)
+        cohortManager.addCohort(cohort2)
+        const expected = "Error 404, this cohort could not be found"
+        const result = cohortManager.findCohort("cohort3")
+        expect(result).toEqual(expected);
+    });
+
     it("Add student to a cohort", () => {
         const expected = [{
             studentId: 1,
@@ -66,6 +74,32 @@ describe("Cohort", () => {
         cohortManager.removeCohort("cohort1")
         const expected = "cohort2 + cohort3"
         const result = cohortManager.findCohort("cohort2").name + " + " + cohortManager.findCohort("cohort3").name  
+        expect(result).toEqual(expected);
+    });
+
+    it("Remove student from a specific cohort", () => {
+        cohortManager.addCohort(cohort1)
+        cohortManager.addCohort(cohort2)
+        cohortManager.addStudent("cohort1", 1, "jacob", "smith", "smithyJ", "smithyJ@gmail.com")
+        cohortManager.addStudent("cohort1", 2, "jason", "samoa", "jazzyS", "jazzyS@gmail.com")
+        const expected = [{
+            studentId: 2,
+            firstName: "jason",
+            lastName: "samoa",
+            githubUsername: "jazzyS",
+            email: "jazzyS@gmail.com"
+        }]
+        const result = cohortManager.removeStudent("cohort1", 1, "jacob", "smith", "smithyJ", "smithyJ@gmail.com")
+        expect(result).toEqual(expected);
+    });
+
+    it("Error message if student cannot be found", () => {
+        cohortManager.addCohort(cohort1)
+        cohortManager.addCohort(cohort2)
+        cohortManager.addStudent("cohort1", 1, "jacob", "smith", "smithyJ", "smithyJ@gmail.com")
+        cohortManager.addStudent("cohort1", 2, "jason", "samoa", "jazzyS", "jazzyS@gmail.com")
+        const expected = "Error 404, this student could not be found"
+        const result = cohortManager.removeStudent("cohort1", 1, "jill", "smith", "smithyJ", "smithyJ@gmail.com")
         expect(result).toEqual(expected);
     });
 
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -5,17 +5,29 @@ class CohortManager {
     this.cohorts = []
   }
 
-  addCohort(name) {
-    this.cohorts.push(name)
+  addCohort(cohortName) {
+    this.cohorts.push(cohortName)
 
     return this.cohorts
   }
 
   findCohort(cohortName) {
+    if (
+      this.cohorts.find((element) => element.name === cohortName) === undefined
+    ) {
+      return 'Error 404, this cohort could not be found'
+    }
+
     return this.cohorts.find((element) => element.name === cohortName)
   }
 
   removeCohort(cohortName) {
+    if (
+      this.cohorts.find((element) => element.name === cohortName) === undefined
+    ) {
+      return 'Error 404, this cohort could not be found'
+    }
+
     this.cohorts = this.cohorts.filter((element) => element.name !== cohortName)
 
     return this.cohorts
@@ -28,6 +40,12 @@ class CohortManager {
       lastName: surname,
       githubUsername: github,
       email: emailAddress
+    }
+
+    if (
+      this.cohorts.find((element) => element.name === cohortName) === undefined
+    ) {
+      return 'Error 404, this cohort could not be found'
     }
 
     const cohortToAddTo = this.findCohort(cohortName).cohort
@@ -47,20 +65,44 @@ class CohortManager {
 
   // The below can search all students matching name then filter by cohortname to ensure the right one
 
-  // removeStudent(cohortName, id, firstName, surname, github, emailAddress) {
-  //   const student = {
-  //     studentId: id,
-  //     firstName: firstName,
-  //     lastName: surname,
-  //     githubUsername: github,
-  //     email: emailAddress
+  removeStudent(cohortName, id, firstName, surname) {
+    if (
+      this.cohorts.find((element) => element.name === cohortName) === undefined
+    ) {
+      return 'Error 404, this cohort could not be found'
+    }
+
+    let cohortToRemoveFrom = this.findCohort(cohortName).cohort
+
+    if (
+      cohortToRemoveFrom.find((element) => element.studentId === id) ===
+        undefined ||
+      cohortToRemoveFrom.find((element) => element.firstName === firstName) ===
+        undefined ||
+      cohortToRemoveFrom.find((element) => element.lastName === surname) ===
+        undefined
+    ) {
+      return 'Error 404, this student could not be found'
+    }
+
+    cohortToRemoveFrom = cohortToRemoveFrom.filter(
+      (element) =>
+        element.studentId !== id &&
+        element.firstName !== firstName &&
+        element.lastName !== surname
+    )
+
+    return cohortToRemoveFrom
+  }
+
+  // studentSearchById(id) {
+  //   let studentsCohort = {}
+
+  //   for (let i = 0; i < this.cohorts.length; i++) {
+  //     studentsCohort = this.cohorts[i].cohort.find((element) => element.studentId === id)
   //   }
 
-  //   const cohortToAddTo = this.findCohort(cohortName).cohort
-
-  //   cohortToAddTo.push(student)
-
-  //   return cohortToAddTo
+  //   return studentsCohort
   // }
 }
 
